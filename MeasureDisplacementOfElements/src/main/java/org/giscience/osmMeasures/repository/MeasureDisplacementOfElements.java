cf5902d2da6585bb8f8536de014d04fc3006de23
Yajie <yajie.liang@stud.uni-heidelberg.de>
2018-11-14 18:50:57 +0100

    test displacement

---
+++ MeasureDisplacementOfElements/src/main/java/org/giscience/osmMeasures/repository/MeasureDisplacementOfElements.java
@@ -0,0 +1,50 @@
+package org.giscience.osmMeasures.repository;
+
+import com.vividsolutions.jts.geom.Coordinate;
+import org.giscience.measures.rest.measure.MeasureOSHDB;
+import org.giscience.measures.rest.server.OSHDBRequestParameter;
+import org.giscience.measures.rest.server.RequestParameter;
+import org.giscience.measures.tools.Cast;
+import org.giscience.measures.tools.Index;
+import org.giscience.measures.tools.Lineage;
+import org.giscience.utils.geogrid.cells.GridCell;
+import org.heigit.bigspatialdata.oshdb.api.mapreducer.MapAggregator;
+import org.heigit.bigspatialdata.oshdb.api.mapreducer.OSMContributionView;
+import org.heigit.bigspatialdata.oshdb.api.object.OSMContribution;
+import org.heigit.bigspatialdata.oshdb.api.object.OSMEntitySnapshot;
+import org.heigit.bigspatialdata.oshdb.osm.OSMType;
+import org.heigit.bigspatialdata.oshdb.util.celliterator.ContributionType;
+import org.heigit.bigspatialdata.oshdb.util.geometry.Geo;
+import org.heigit.bigspatialdata.oshdb.util.tagInterpreter.DefaultTagInterpreter;
+import org.heigit.bigspatialdata.oshdb.util.tagInterpreter.TagInterpreter;
+
+import java.util.SortedMap;
+
+public class MeasureDisplacementOfElements extends MeasureOSHDB<Number, OSMContribution> {
+
+    @Override
+    public Boolean refersToTimeSpan() {
+        return false;
+    }
+
+    @Override
+    public SortedMap<GridCell, Number> compute(MapAggregator<GridCell, OSMContribution> mapReducer, OSHDBRequestParameter p) throws Exception {
+        return Index.reduce(
+                mapReducer
+                        .osmTag(p.getOSMTag())
+                        .aggregateBy(contribution -> contribution.getEntityAfter().getId())
+                        .filter(snapshot -> {
+                            return  snapshot.getContributionTypes().contains(ContributionType.GEOMETRY_CHANGE)&&
+                                    // exclude exaggerated change
+                                    (Geo.areaOf(snapshot.getGeometryAfter()) / Geo.areaOf(snapshot.getGeometryBefore()) <= 10);
+                        })
+                        .sum(contribution -> {
+                            Coordinate before = contribution.getGeometryBefore().getCentroid().getCoordinate();
+                            Coordinate after = contribution.getGeometryAfter().getCentroid().getCoordinate();
+                            com.vividsolutions.jts.geom.GeometryFactory gf = new com.vividsolutions.jts.geom.GeometryFactory();
+                            return Geo.lengthOf(gf.createLineString(new Coordinate[] { before, after }));
+
+                        }),
+                Lineage::average);
+    }
+}
---
+++ MeasureNotSimplePolygon/src/main/java/org/giscience/osmMeasures/repository/FakeTagInterpreter.java
@@ -0,0 +1,50 @@
+package org.giscience.osmMeasures.repository;
+
+import org.heigit.bigspatialdata.oshdb.osm.OSMEntity;
+import org.heigit.bigspatialdata.oshdb.osm.OSMMember;
+import org.heigit.bigspatialdata.oshdb.osm.OSMRelation;
+import org.heigit.bigspatialdata.oshdb.util.tagInterpreter.TagInterpreter;
+
+import java.util.Map;
+import java.util.Set;
+
+class FakeTagInterpreter implements TagInterpreter {
+    int areaNoTagKeyId, areaNoTagValueId;
+    Map<Integer, Set<Integer>> wayAreaTags;
+    Map<Integer, Set<Integer>> relationAreaTags;
+    Set<Integer> uninterestingTagKeys;
+    int outerRoleId, innerRoleId, emptyRoleId;
+
+    FakeTagInterpreter(
+            int areaNoTagKeyId,
+            int areaNoTagValueId,
+            Map<Integer, Set<Integer>> wayAreaTags,
+            Map<Integer, Set<Integer>> relationAreaTags,
+            Set<Integer> uninterestingTagKeys,
+            int outerRoleId,
+            int innerRoleId,
+            int emptyRoleId
+    ) {
+        this.areaNoTagKeyId = areaNoTagKeyId;
+        this.areaNoTagValueId = areaNoTagValueId;
+        this.wayAreaTags = wayAreaTags;
+        this.relationAreaTags = relationAreaTags;
+        this.uninterestingTagKeys = uninterestingTagKeys;
+        this.outerRoleId = outerRoleId;
+        this.innerRoleId = innerRoleId;
+        this.emptyRoleId = emptyRoleId;
+    }
+    @Override
+    public boolean isArea(OSMEntity entity) { return false; }
+    @Override
+    public boolean isLine(OSMEntity entity) { return true; }
+    @Override
+    public boolean hasInterestingTagKey(OSMEntity osm) { return false; }
+    @Override
+    public boolean isMultipolygonOuterMember(OSMMember osmMember) { return false; }
+    @Override
+    public boolean isMultipolygonInnerMember(OSMMember osmMember) { return false; }
+    @Override
+    public boolean isOldStyleMultipolygon(OSMRelation osmRelation) { return false; }
+
+}
