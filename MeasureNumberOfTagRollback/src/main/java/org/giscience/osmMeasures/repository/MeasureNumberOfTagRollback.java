8183dd094c56a7db672813f0a85a03d5e8764191
Yajie <yajie.liang@stud.uni-heidelberg.de>
2019-01-28 14:16:17 +0100

    update

--- MeasureNumberOfTagRollback/src/main/java/org/giscience/osmMeasures/repository/MeasureNumberOfTagRollback.java
+++ MeasureNumberOfTagRollback/src/main/java/org/giscience/osmMeasures/repository/MeasureNumberOfTagRollback.java
@@ -68,7 +68,7 @@ public class MeasureNumberOfTagRollback extends MeasureOSHDB<Number, OSMEntitySn
     public SortedMap<GridCell, Number> compute(MapAggregator<GridCell, OSMEntitySnapshot> mapReducer, OSHDBRequestParameter p) throws Exception {
         return Cast.result(mapReducer
                 .osmTag("highway")
-                //.count());
+                .count());
                 /*.groupByEntity()
                 // can be replaced by .aggregateBy(contribution -> contribution.getEntityAfter())?
                 .flatMap(contributions -> {
