1a9820575da4dc547b7e0a02746622e391b889c5
Yajie <yajie.liang@stud.uni-heidelberg.de>
2019-06-02 12:33:39 +0200

    update

--- MeasurePolygonCompactness/src/main/java/org/giscience/osmMeasures/repository/MeasurePolygonCompactness.java
+++ MeasurePolygonCompactness/src/main/java/org/giscience/osmMeasures/repository/MeasurePolygonCompactness.java
@@ -5,23 +5,22 @@ import org.giscience.measures.rest.measure.MeasureOSHDB;
 import org.giscience.measures.rest.server.OSHDBRequestParameter;
 import org.giscience.measures.rest.server.RequestParameter;
 import org.giscience.measures.tools.Cast;
-import org.giscience.measures.tools.Index;
-import org.giscience.measures.tools.Lineage;
 import org.giscience.utils.geogrid.cells.GridCell;
 import org.heigit.bigspatialdata.oshdb.api.mapreducer.MapAggregator;
 import org.heigit.bigspatialdata.oshdb.api.object.OSMEntitySnapshot;
+import org.heigit.bigspatialdata.oshdb.osm.OSMType;
 import org.heigit.bigspatialdata.oshdb.util.geometry.Geo;
 
 import java.util.SortedMap;
 
-public class MeasureNodesDensity extends MeasureOSHDB<Number, OSMEntitySnapshot> {
+public class MeasurePolygonCompactness extends MeasureOSHDB<Number, OSMEntitySnapshot> {
 
-
+/*
     @Override
     public Boolean refersToTimeSpan() {
         return false;
     }
-    /*
+
     @Override
     public Integer defaultDaysBefore() {
         return 3 * 12 * 30;
@@ -35,16 +34,18 @@ public class MeasureNodesDensity extends MeasureOSHDB<Number, OSMEntitySnapshot>
 
     @Override
     public SortedMap<GridCell, Number> compute(MapAggregator<GridCell, OSMEntitySnapshot> mapReducer, OSHDBRequestParameter p) throws Exception {
-        return Cast.result(
-                mapReducer
-                        .osmTag(p.getOSMTag())
-                        .filter(snapshot -> snapshot.getGeometryUnclipped().getNumPoints() > 5)
-                        // make sure is polygon
-                        .filter(snapshot -> snapshot.getGeometry().getDimension()==1)
-                        .map(snapshot -> {
-                            Geometry g = snapshot.getGeometryUnclipped();
-                            return Geo.areaOf(g)/g.getNumPoints();})
-                        .average());
-
+        return Cast.result(mapReducer
+                .osmType(OSMType.WAY)
+                .filter(snapshot -> snapshot.getGeometryUnclipped().getNumPoints() > 4)
+                // because we exclude triangle, which contains exactly 4 nodes
+                .filter(snapshot -> {
+                    // implemented according to Polsby-Popper Test
+                    Geometry g = snapshot.getGeometryUnclipped();
+                    Double perimeter = Geo.lengthOf(g.getBoundary());
+                    Double PPD = 4*Geo.areaOf(g)/(perimeter*perimeter);
+                    // PPD close to 1 means the polygon has great compactness
+                    return PPD < 0.1;
+                })
+                .count());
     }
 }
